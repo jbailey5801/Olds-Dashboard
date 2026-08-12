f1c2ecec835f42a388ff098a8342948737746692
jbailey5801 <jbailey5801@gmail.com>
2026-08-12 12:40:31 -0700

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("🌎 World Intelligence Dashboard")
+
+st.header("📈 Long-Term Trends")
+st.write("Our World in Data")
+
+st.header("🏗 Progress & Institutions")
+st.write("Works in Progress")
+
+st.header("⚙ Technology & Industry")
+st.write("Noahpinion")
+st.write("MIT Technology Review")
+
+st.header("🌍 Geopolitics")
+st.write("The Economist")
+st.write("Foreign Affairs")
