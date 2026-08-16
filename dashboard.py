bf6066b884cd15c23921f9ad14e973c93429f03c
jbailey5801 <jbailey5801@gmail.com>
2026-08-16 07:28:13 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import feedparser
 
-st.title("OWID Feed Test")
-
 url = "https://ourworldindata.org/feed.xml"
 
 feed = feedparser.parse(url)
 
-st.write("Feed Version:", feed.get("version"))
+st.write("Version:", feed.get("version"))
 st.write("Bozo:", feed.bozo)
-st.write("Number of Entries:", len(feed.entries))
+st.write("Entries:", len(feed.entries))
+st.write("Status:", getattr(feed, "status", "No status"))
 
-for article in feed.entries[:5]:
-    st.write(article.title)
+if hasattr(feed, "bozo_exception"):
+    st.write("Error:")
+    st.write(feed.bozo_exception)
