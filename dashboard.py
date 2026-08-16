e57c7548fdde58e80c2fa01d62e9d157208355cb
jbailey5801 <jbailey5801@gmail.com>
2026-08-16 07:30:13 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,9 @@
-import streamlit as st
-import urllib.request
-
-url = "https://ourworldindata.org/feed.xml"
-
-try:
-    response = urllib.request.urlopen(url)
-
-    content = response.read().decode(
-        "utf-8",
-        errors="ignore"
-    )
-
-    st.write(content[:2000])
-
-except Exception as e:
-    st.error(e)
+feed = feedparser.parse(
+2
+"https://ourworldindata.org/feed"
+3
+)
+4
+ 
+5
+st.write("Entries:", len(feed.entries))
